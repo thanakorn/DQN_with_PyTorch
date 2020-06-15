072a9cf590d77762a862d1420979f994124d10e1
thanakorn <thanakorn.panyapiang@outlook.com>
2020-06-15 12:04:33 +0100

    Pre-process image

--- env_manager/cartpole_env_manager.py
+++ env_manager/cartpole_env_manager.py
@@ -1,4 +1,9 @@
 import gym
+import torch
+import numpy as np
+import torchvision.transforms as T
+
+from PIL import Image
 
 class CartPoleEnvManager(object):
     def __init__(self, device):
@@ -7,6 +12,7 @@ class CartPoleEnvManager(object):
         self.env.reset()
         self.current_screen = None
         self.done = False
+        self.device = device
         
     def reset(self):
         self.env.reset()
@@ -17,3 +23,48 @@ class CartPoleEnvManager(object):
         
     def render(self, mode='human'):
         return self.env.render(mode)
+    
+    def num_actions(self):
+        return self.env.action_space.n
+    
+    def take_action(self, action):
+        _, reward, self.done, _ = self.env.step(action)
+        return torch.tensor([reward], device=self.device)
+    
+    def just_start(self):
+        return self.current_screen is None
+    
+    def get_state(self):
+        if self.just_start() or self.done:
+            self.current_screen = self.get_processed_screen()
+            empty_screen = torch.zeros_like(self.current_screen)
+            return empty_screen
+        else:
+            s1 = self.current_screen
+            s2 = self.get_processes_screen()
+            self.current_screen = s2
+            return s1 - s2
+        
+    def get_processed_screen(self):
+        screen = self.render('rbg_array').transpose((2, 0, 1))
+        screen = self.crop_screen(screen)
+        return self.transform_screen(screen)
+    
+    def crop_screen(self, screen):
+        height = screen.shape[1]
+        top = int(height * 0.4)
+        bottom = int(height * 0.8)
+        screen = screen[:, top:bottom, :]
+        return screen
+    
+    def transform_screen(self, screen):
+        screen = np.ascontiguousarray(screen, dtype=np.float32) / 255.
+        screen = torch.from_numpy(screen)
+        
+        resize = T.Compose([
+            T.ToPILImage(),
+            T.Resize((40, 90)),
+            T.ToTensor()
+        ])
+        
+        return resize(screen).unsqueeze(0).to(self.device)
